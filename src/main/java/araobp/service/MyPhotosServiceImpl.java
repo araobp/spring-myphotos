059a91f518932e484c1945ef3be2982966967aa4
araobp <araobp@users.noreply.github.com>
2022-02-17 21:40:00 +0900

    Create MyPhotosServiceImpl.java

---
+++ src/main/java/araobp/service/MyPhotosServiceImpl.java
@@ -0,0 +1,37 @@
+package araobp.service;
+
+import java.util.Optional;
+
+public class MyPhotosServiceImpl implements MyPhotosService {
+
+	@Override
+	public Iterable<Record> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Record> selectOneById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertRecord(Record record) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateRecord(Record record) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRecordById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
