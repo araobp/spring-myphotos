a16c61bbe60b999d1f6683d530ecc26f7ac1d538
araobp <araobp@users.noreply.github.com>
2022-02-18 22:19:44 +0900

    update

--- src/main/java/araobp/controller/MyPhotosRestController.java
+++ src/main/java/araobp/controller/MyPhotosRestController.java
@@ -1,8 +1,13 @@
 package araobp.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import araobp.domain.entity.Record;
 import araobp.domain.service.MyPhotosService;
@@ -13,10 +18,20 @@ public class MyPhotosRestController {
 	@Autowired
 	MyPhotosService service;
 	
-	@GetMapping("/records")
+	@GetMapping("/record")
 	public Iterable<Record> getRecords() {
 		Iterable<Record> records = service.selectAll();
 		return records;
 	}
+	
+	@GetMapping("/record/{id}")
+	public Optional<Record> getRecord(@PathVariable Integer id) {
+		Optional<Record> record = service.selectOneById(id);
+		if (record.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		return record;
+	}
+	
 
 }
