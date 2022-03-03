1d09ca01a65720005cae57883090afd2b890fe47
araobp <araobp@users.noreply.github.com>
2022-03-04 07:27:14 +0900

    Update RecordAndPhotoServiceImpl.java

--- src/main/java/araobp/domain/service/RecordAndPhotoServiceImpl.java
+++ src/main/java/araobp/domain/service/RecordAndPhotoServiceImpl.java
@@ -114,8 +114,12 @@ public class RecordAndPhotoServiceImpl implements RecordAndPhotoService {
 			int targetHeight = originalImage.getHeight() * THUMBNAIL_TARGET_WIDTH / originalImage.getWidth();
 			try {
 				// Resize image
-				Thumbnails.of(originalImage).size(THUMBNAIL_TARGET_WIDTH, targetHeight).outputFormat("JPEG")
+				//Thumbnails.of(originalImage).size(THUMBNAIL_TARGET_WIDTH, targetHeight).outputFormat("JPEG")
+				//		.outputQuality(1).toOutputStream(outputStream);
+				// Resize image
+				Thumbnails.of(inputStream).size(THUMBNAIL_TARGET_WIDTH, targetHeight).outputFormat("JPEG")
 						.outputQuality(1).toOutputStream(outputStream);
+
 			} catch (IOException e) {
 				e.printStackTrace();
 				return false;
