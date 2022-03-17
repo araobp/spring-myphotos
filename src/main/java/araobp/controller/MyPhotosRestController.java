a72c5594b218e53f77306ace562f7e6207f65f2e
araobp <araobp@users.noreply.github.com>
2022-03-17 16:07:00 +0900

    Update MyPhotosRestController.java

--- src/main/java/araobp/controller/MyPhotosRestController.java
+++ src/main/java/araobp/controller/MyPhotosRestController.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,7 +48,7 @@ public class MyPhotosRestController {
 		return recordAndPhotoService.insertRecord(record);
 	}
 	
-	@PutMapping("/record/{id}")
+	@PatchMapping("/record/{id}")
 	public void putRecord(@PathVariable Integer id, @RequestBody Record record) {
 		Boolean success = recordAndPhotoService.updateRecord(id, record.getPlace(), record.getMemo());
 		if (!success) throw new ResponseStatusException(HttpStatus.NOT_FOUND, NOT_FOUND_REASON);
